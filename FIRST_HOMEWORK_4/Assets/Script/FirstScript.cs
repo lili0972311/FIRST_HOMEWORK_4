b8574d3df284d9b312818e1d69e2cc5d63a2587b
陳昕儀 <113577945+lili0972311@users.noreply.github.com>
2022-10-13 16:00:11 +0800

    2022_10_13
    調整重力，碰撞範圍，跟寫程式

---
+++ FIRST_HOMEWORK_4/Assets/Script/FirstScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace KUMO
+{
+    /// <summary>
+    /// 第一個程式
+    /// </summary>
+    public class FirstScript : MonoBehaviour
+    {
+        #region   
+        // 縮排 快捷鍵 Tab
+        // 格式化文件 Ctrl + K D
+        // 單行註解
+
+        /*
+         * 多行註解
+         * 多行註解
+        */
+        #endregion
+        #region 事件區域
+        // 喚醒事件 : 遊戲開始時執行一次
+        private void Awake()
+        {
+            
+        }
+        // 開始事件 : 在 Awake 後執行一次
+        private void Start()
+        {
+            
+        }
+        #endregion
+    }
+}
